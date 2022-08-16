245ab71d932b5ad173ff803e85b8291f58234c83
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-08-16 13:40:14 +0430

    The structure of project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import { PureComponent } from "react";
-import Header from "./Dashboard/Header/Header.jsx";
-import SearchBar from "./Dashboard/Searchbar/SearchBar.jsx";
-import EmojiResults from "./Dashboard/Emojis/EmojiResults";
-import Filter from "./Dashboard/Emojis/Filter";
-export default class App extends PureComponent {
-  state = {
-    filteredEmojis: Filter("", 20),
-  };
+import React from 'react'
 
-  handleChange = (event) => {
-    this.setState({
-      filteredEmojis: Filter(event.target.value, 15),
-    });
-  };
-
-  render() {
+function App() {
     return (
-      <div>
-        <Header />
-        <SearchBar handleChange={this.handleChange} />
-        <EmojiResults emojiData={this.state.filteredEmojis} />
-      </div>
-    );
-  }
+        <div>Apppppppppp</div>
+    )
 }
+
+export default App
